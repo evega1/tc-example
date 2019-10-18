56d3f13f38f91633b6c5c46040d4b808a7968e3a
Edwin V <edwin.vega@wizeline.com>
2019-10-18 15:45:59 -0500

    Updated selectors of StartPage

--- page_objects/StartPage.js
+++ page_objects/StartPage.js
@@ -4,13 +4,11 @@ class StartPage {
 
     constructor() {
         this.topBar = Selector('#top_bar');
-        this.addTaskButton = Selector('.ist_button_red');
         this.taskTextInput = Selector('.public-DraftEditor-content');
         this.taskScheduleInput = Selector('.item_editor_assign_due');
         this.taskDateInput = Selector('.scheduler-input');
         this.taskDatePreview = Selector('.scheduler-preview-content');
-        this.taskSubmitButton = Selector('.item_editor_submit.ist_button.ist_button_red');
-        this.inboxButton = Selector('#filter_inbox');
+        this.taskSubmitButton = Selector('.ist_button_red');
         this.taskItems = Selector('.sel_item_content');
         this.newTaskBtn = Selector('#quick_add_task_holder');
     }
